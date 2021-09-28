c244088c025be44c6583a2751eaf67ed75a7c58f
lasha tatulashvili <lashatatu@gmail.com>
2021-09-28 16:30:07 +0400

    42 cart start

--- keystone.ts
+++ keystone.ts
@@ -1,14 +1,18 @@
-import 'dotenv/config';
-import { config, createSchema } from '@keystone-next/keystone/schema';
-import { User } from './schemas/User';
-import { createAuth } from '@keystone-next/auth';
-import { statelessSessions, withItemData } from '@keystone-next/keystone/session';
-import { Product } from './schemas/Product';
-import { ProductImage } from './schemas/ProductImage';
-import { insertSeedData } from './seed-data';
+import "dotenv/config";
+import { config, createSchema } from "@keystone-next/keystone/schema";
+import { User } from "./schemas/User";
+import { createAuth } from "@keystone-next/auth";
+import {
+  statelessSessions,
+  withItemData,
+} from "@keystone-next/keystone/session";
+import { Product } from "./schemas/Product";
+import { ProductImage } from "./schemas/ProductImage";
+import { insertSeedData } from "./seed-data";
+import { sendPasswordResetEmail } from "./lib/mail";
 
 const databaseURL =
-  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+  process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
 
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360,
@@ -16,15 +20,15 @@ const sessionConfig = {
 };
 
 const { withAuth } = createAuth({
-  listKey: 'User',
-  identityField: 'email',
-  secretField: 'password',
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
   initFirstItem: {
-    fields: ['name', 'email', 'password'],
+    fields: ["name", "email", "password"],
   },
-  passwordResetLink:{
-    async sendToken (args) {
-      console.log(args);
+  passwordResetLink: {
+    async sendToken(args) {
+      await sendPasswordResetEmail(args.token, args.identity);
     },
   },
 });
@@ -38,10 +42,10 @@ export default withAuth(
       },
     },
     db: {
-      adapter: 'mongoose',
+      adapter: "mongoose",
       url: databaseURL,
-      async onConnect (keystone) {
-        if ( process.argv.includes('--seed-data') ) {
+      async onConnect(keystone) {
+        if (process.argv.includes("--seed-data")) {
           await insertSeedData(keystone);
         }
       },
@@ -59,5 +63,5 @@ export default withAuth(
     session: withItemData(statelessSessions(sessionConfig), {
       User: `id`,
     }),
-  }),
+  })
 );
