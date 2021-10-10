f3d9909828c9f7f171bb4c463c4a33f789067c69
lasha tatulashvili <lashatatu@gmail.com>
2021-10-10 20:57:51 +0400

    60 displaying all orders

--- mutations/checkout.ts
+++ mutations/checkout.ts
@@ -84,7 +84,7 @@ async function checkout(
       user: { connect: { id: userId } },
     },
   });
-  const cartItemIds = cartItems.map((cartItem) => cartItem.id);
+  const cartItemIds = user.cart.map((cartItem) => cartItem.id);
   await context.lists.CartItem.deleteMany({
     ids: cartItemIds,
   });
