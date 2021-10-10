13038099dbaaff31b4aada2fd22783508f2d4075
lasha tatulashvili <lashatatu@gmail.com>
2021-10-10 15:04:22 +0400

    55 custom checkout mutation with stripe

--- keystone.ts
+++ keystone.ts
@@ -11,7 +11,9 @@ import { Product } from "./schemas/Product";
 import { ProductImage } from "./schemas/ProductImage";
 import { insertSeedData } from "./seed-data";
 import { sendPasswordResetEmail } from "./lib/mail";
-import { extendGraphqlSchema } from './mutations';
+import { extendGraphqlSchema } from "./mutations";
+import { OrderItem } from "./schemas/OrderItem";
+import { Order } from './schemas/Order';
 
 const databaseURL =
   process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
@@ -57,6 +59,8 @@ export default withAuth(
       Product,
       ProductImage,
       CartItem,
+      OrderItem,
+      Order
     }),
     extendGraphqlSchema,
     ui: {
