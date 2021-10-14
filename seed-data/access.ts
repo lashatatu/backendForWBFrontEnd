8dd38e479db6915bbd22c1fcbc72f4a78abb9c8e
lasha tatulashvili <lashatatu@gmail.com>
2021-10-14 06:13:18 +0400

    65 more flexible rules

--- seed-data/access.ts
+++ seed-data/access.ts
@@ -1,5 +1,22 @@
-import { ListAccessArgs } from '../types';
+import { ListAccessArgs } from "../types";
+import { permissionsList } from "../schemas/fields";
 
-export function isSignedIn({ session }:ListAccessArgs) {
+export function isSignedIn({ session }: ListAccessArgs) {
   return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs) {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+export const permissions = {
+  ...generatedPermissions,
+  isAwesome({ session }: ListAccessArgs) {
+    return session?.data.name.includes("lasha");
+  },
+};
