ae1cfeaa86e94644146f6516037cb027a3bf3696
lasha tatulashvili <lashatatu@gmail.com>
2021-10-14 06:43:48 +0400

    68 user permission

--- schemas/Order.ts
+++ schemas/Order.ts
@@ -1,17 +1,25 @@
-import { integer, text, virtual,  relationship,} from '@keystone-next/fields';
-import { list } from '@keystone-next/keystone/schema';
-import formatMoney from '../lib/formatMoney';
+import { integer, relationship, text, virtual } from "@keystone-next/fields";
+import { list } from "@keystone-next/keystone/schema";
+import formatMoney from "../lib/formatMoney";
+import { isSignedIn, rules } from "../seed-data/access";
 
 export const Order = list({
+  access: {
+    create: isSignedIn,
+    read: rules.canOrder,
+    update: () => false,
+    delete: () => false,
+  },
   fields: {
     label: virtual({
-      graphQLReturnType: 'String', resolver: function(item) {
+      graphQLReturnType: "String",
+      resolver: function (item) {
         return `lt is cool ${formatMoney(item.total)}`;
       },
     }),
     total: integer(),
-    items: relationship({ ref: 'OrderItem.order', many: true }),
-    user: relationship({ ref: 'User.orders' }),
+    items: relationship({ ref: "OrderItem.order", many: true }),
+    user: relationship({ ref: "User.orders" }),
     charge: text(),
   },
 });
